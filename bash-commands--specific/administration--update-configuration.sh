5f9c9e1ef800363b892d0491a95c3826a21f71de
David Lechner <david.lechner@talsen.team>
2019-05-19 14:13:29 +0200

    refine repo specific command execution

--- bash-commands--specific/administration--update-configuration.sh
+++ bash-commands--specific/administration--update-configuration.sh
@@ -6,13 +6,12 @@ source bash-util/functions.sh
 
 prepare_local_environment ${@}
 
-VAR_NAME_OF_SERVICE="${3}"
-VAR_NAME_OF_CONFIGURATION="${4}"
+VAR_NAME_OF_CONFIGURATION="${3}"
 
 echo -E "Updating NGINX configuration '${VAR_NAME_OF_CONFIGURATION}' ..."
 
-docker exec --tty                  \
-            ${VAR_NAME_OF_SERVICE} \
+docker exec --tty                \
+            ${HOST_SERVICE_NAME} \
                     /bin/bash /opt/server-nginx-certbot/command-update-configuration.sh "${VAR_NAME_OF_CONFIGURATION}"
 
 echo -e "Updating NGINX configuration '${VAR_NAME_OF_CONFIGURATION}' ... $( __done )"
