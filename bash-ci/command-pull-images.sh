d4d9bf8692a9c408158d3ef68451ff99ba1386a2
David Lechner <david.lechner@talsen.team>
2019-11-06 13:44:57 +0100

    clean workspace

--- bash-ci/command-pull-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo -e "Performing pull ..."
-
-/bin/bash bash-commands/docker-compose--image--pull.sh "." "default.docker-compose"
-
-echo -e "Performing pull ... \033[0;32mdone\033[0m"
--- docker/server--nginx-certbot/rootfs/opt/server--nginx-certbot/test-nginx-configuration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-nginx -t
-
-if [ ${?} -eq 0 ];then
-    echo "true"
-else
-    echo "false"
-fi
