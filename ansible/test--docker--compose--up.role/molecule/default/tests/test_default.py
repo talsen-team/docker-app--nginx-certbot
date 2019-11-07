b6123400d0f22d5bf656d788c003b428ec181f28
David Lechner <david.lechner@talsen.team>
2019-11-07 18:40:03 +0100

    remove unnecessary test cases

--- ansible/test--docker--compose--up.role/molecule/default/tests/test_default.py
+++ ansible/test--docker--compose--up.role/molecule/default/tests/test_default.py
@@ -1,4 +1,3 @@
-import pytest
 import os
 
 import testinfra.utils.ansible_runner
@@ -9,21 +8,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 configuration = '127.0.0.1:80'
 expected_container_name = 'server--nginx-certbot'
 test_dir = os.environ['TESTS_DIR'] + '/' + os.environ['ROLE_NAME']  # noqa: #501
-
-
-@pytest.mark.parametrize('name', [
-    f'/bash/util/functions.bash',
-    f'/bash/docker/compose/up.bash',
-    f'/docker/{ configuration }/default.docker',
-    f'/docker/{ configuration }/default.docker-compose',
-    f'/env/{ configuration }/docker-containers.env',
-    f'/env/{ configuration }/docker-images.env',
-])
-def test_that_required_files_are_existing(host, name):
-    f = host.file(test_dir + name)
-
-    assert f.exists
-    assert not f.is_directory
 
 
 def test_that_required_docker_containers_are_running(host):
--- ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
+++ ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
@@ -1,4 +1,3 @@
-import pytest
 import os
 
 import testinfra.utils.ansible_runner
@@ -9,20 +8,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 configuration = '127.0.0.1:80'
 expected_image_name = 'talsenteam/docker-nginx-certbot:v1.17.4'
 test_dir = os.environ['TESTS_DIR'] + '/' + os.environ['ROLE_NAME']  # noqa: #501
-
-
-@pytest.mark.parametrize('name', [
-    f'/bash/util/functions.bash',
-    f'/bash/docker/image/build.bash',
-    f'/docker/{ configuration }/default.docker',
-    f'/docker/{ configuration }/default.docker-compose',
-    f'/env/{ configuration }/docker-images.env',
-])
-def test_that_required_files_are_existing(host, name):
-    f = host.file(test_dir + name)
-
-    assert f.exists
-    assert not f.is_directory
 
 
 def test_that_required_docker_image_exists(host):
