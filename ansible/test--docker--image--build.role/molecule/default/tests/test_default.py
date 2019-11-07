a9c662b08e63ff66fd92f019ae917584b4573893
David Lechner <david.lechner@talsen.team>
2019-11-07 09:42:56 +0100

    add test constraints

--- ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
+++ ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
@@ -11,10 +11,20 @@ test_dir = os.environ['TESTS_DIR'] + '/' + os.environ['ROLE_NAME']  # noqa: #501
 
 @pytest.mark.parametrize('name', [
     '/bash/docker/image/build.bash',
+    '/env/127.0.0.1:80/docker-images.env',
 ])
 def test_that_required_files_are_existing(host, name):
-
     f = host.file(test_dir + name)
 
     assert f.exists
     assert not f.is_directory
+
+
+def test_that_required_docker_image_exists(host):
+    x = host.run('cat ' + test_dir + '/env/127.0.0.1:80/docker-images.env')  # noqa: #501
+
+    expected_image_name = x.stdout.splitlines()[0].split('=', 2)[1]
+
+    c = host.run('docker images ' + expected_image_name + ' --format "{{.Repository}}:{{.Tag}}"')  # noqa: #501
+
+    assert expected_image_name + '\n' == c.stdout
