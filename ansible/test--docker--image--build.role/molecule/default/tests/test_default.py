28e78b503bafcad258ae775bfc800d3c3d2772c8
David Lechner <david.lechner@talsen.team>
2019-11-07 10:20:28 +0100

    refine implementation

--- ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
+++ ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
@@ -10,7 +10,10 @@ test_dir = os.environ['TESTS_DIR'] + '/' + os.environ['ROLE_NAME']  # noqa: #501
 
 
 @pytest.mark.parametrize('name', [
+    '/bash/util/functions.bash',
     '/bash/docker/image/build.bash',
+    '/docker/127.0.0.1:80/default.docker',
+    '/docker/127.0.0.1:80/default.docker-compose',
     '/env/127.0.0.1:80/docker-images.env',
 ])
 def test_that_required_files_are_existing(host, name):
---
+++ bash/docker/image/build.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source bash/util/functions.bash
+
+PROJECT_DIR=$( realpath . )
+
+function export_env_docker_images() {
+    local PATH_TO_ENV_FILE=env/${1}/docker-images.env
+
+    while IFS='' read -r LINE || [[ -n ${LINE} ]];
+    do
+        export ${LINE}
+    done < ${PATH_TO_ENV_FILE}
+}
+
+function run() {
+
+    local CONFIGURATION=${1}
+    local DOCKER_CONFIG_DIR=docker/${CONFIGURATION}
+
+    export_env_docker_images ${CONFIGURATION}
+
+    print_title      "Building docker images"
+
+    cd ${DOCKER_CONFIG_DIR}
+
+    docker-compose --file default.docker-compose \
+                   build
+
+    cd ${PROJECT_DIR}
+
+    print_title_done "Building docker images"
+}
+
+run ${@}
