c0d64c252c51d4b7d437a0addae63b1a053d2a8a
David Lechner <david.lechner@talsen.team>
2019-11-07 10:42:44 +0100

    reduce duplication of parameterization

--- ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
+++ ansible/test--docker--image--build.role/molecule/default/tests/test_default.py
@@ -6,15 +6,17 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+configuration = '127.0.0.1:80'
+expected_image_name = 'talsenteam/docker-nginx-certbot:v1.17.4'
 test_dir = os.environ['TESTS_DIR'] + '/' + os.environ['ROLE_NAME']  # noqa: #501
 
 
 @pytest.mark.parametrize('name', [
-    '/bash/util/functions.bash',
-    '/bash/docker/image/build.bash',
-    '/docker/127.0.0.1:80/default.docker',
-    '/docker/127.0.0.1:80/default.docker-compose',
-    '/env/127.0.0.1:80/docker-images.env',
+    f'/bash/util/functions.bash',
+    f'/bash/docker/image/build.bash',
+    f'/docker/{ configuration }/default.docker',
+    f'/docker/{ configuration }/default.docker-compose',
+    f'/env/{ configuration }/docker-images.env',
 ])
 def test_that_required_files_are_existing(host, name):
     f = host.file(test_dir + name)
@@ -24,8 +26,6 @@ def test_that_required_files_are_existing(host, name):
 
 
 def test_that_required_docker_image_exists(host):
-    expected_image_name = 'talsenteam/docker-nginx-certbot:v1.17.4'
-
     c = host.run('docker images ' + expected_image_name + ' --format "{{.Repository}}:{{.Tag}}"')  # noqa: #501
 
     assert expected_image_name + '\n' == c.stdout
