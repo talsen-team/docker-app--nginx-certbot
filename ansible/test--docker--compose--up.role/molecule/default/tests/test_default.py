06a1ab635b2b1198c32e71ce9b1d86601e25fba1
David Lechner <david.lechner@talsen.team>
2019-11-10 22:38:53 +0100

    refine test constraint

--- ansible/test--docker--compose--up.role/molecule/default/tests/test_default.py
+++ ansible/test--docker--compose--up.role/molecule/default/tests/test_default.py
@@ -17,13 +17,18 @@ def test_that_required_docker_containers_are_running(host):
     assert expected_container_name + '\n' == c.stdout
 
 
-@pytest.mark.parametrize('path', [
-    f'{test_dir}/volumes/{expected_container_name}/var/log/backbone/entry/entry.log',  # noqa: #501
-    f'{test_dir}/volumes/{expected_container_name}/var/log/nginx/access.log',
-    f'{test_dir}/volumes/{expected_container_name}/var/log/nginx/error.log',
+@pytest.mark.parametrize('path, content', [
+    [ f'{test_dir}/volumes/{expected_container_name}/var/log/backbone/entry/entry.log', [ 'Started NGINX', 'Entry cascade succeeded' ] ],  # noqa: #501
+    [ f'{test_dir}/volumes/{expected_container_name}/var/log/nginx/access.log', [ ] ],  # noqa: #501
+    [ f'{test_dir}/volumes/{expected_container_name}/var/log/nginx/error.log', [ ] ],  # noqa: #501
 ])
-def test_that_required_files_exist(host, path):
+def test_that_required_files_with_correct_content_exist(host, path, content):
     f = host.file(path)
 
     assert f.exists
     assert not f.is_directory
+
+    fc = f.content_string
+
+    for i in range(len(content)):
+        assert content[i] in fc.splitlines()[i]
